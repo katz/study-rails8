cc7923525ad1835e159b75451d815f43830cd3e9
Katz Sakai <sakai@onflow.jp>
2025-01-29 11:52:51 +0000

    メール送信数テストコードをRSpec化した

--- store/spec/models/product_spec.rb
+++ store/spec/models/product_spec.rb
@@ -1,7 +1,19 @@
 require "rails_helper" # 設定ファイルrails_helper.rbを読み込むコードが全テストにあります
 
 RSpec.describe Product, type: :model do # Productモデルのテストコードをブロック内に書いていきます
+  include ActionMailer::TestHelper
+
   it "trueであるとき、trueになること" do
     expect(true).to eq(true)
+  end
+
+  it "sends email notifications when back in stock" do
+    product = FactoryBot.create(:product, :with_2_subscribers)
+
+    product.update(inventory_count: 0)
+
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
   end
 end
