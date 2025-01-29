e4741ddaff96e5c61f2e797cac3179a8a2b00242
Katz Sakai <sakai@onflow.jp>
2025-01-29 12:22:04 +0000

    ProductMailerのテストコードをRSpec化した

---
+++ store/spec/mailers/product_mailer_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe ProductMailer, type: :mailer do
+  it "trueであるとき、trueになること" do
+    expect(true).to eq(true)
+  end
+
+  it "in_stock" do
+    product = FactoryBot.create(:product)
+    subscriber = FactoryBot.create(:subscriber, product: product, email: "hoge@example.com")
+
+    mail = ProductMailer.with(product: product, subscriber: subscriber).in_stock
+
+    expect(mail.subject).to eq("In stock")
+    expect(mail.to).to eq([ "hoge@example.com" ])
+    expect(mail.from).to eq([ "from@example.com" ])
+    expect(mail.body.encoded).to match("Good news!")
+  end
+end
