32689d274bd6ade3ee87e6760dd28e8abb57925f
Katz Sakai <sakai@onflow.jp>
2025-01-29 10:50:02 +0000

    Productモデルのテストコードを書くファイルを準備した

---
+++ store/spec/models/product_spec.rb
@@ -0,0 +1,6 @@
+require "rails_helper" # 設定ファイルrails_helper.rbを読み込むコードが全テストにあります
+
+RSpec.describe Product, type: :model do # Productモデルのテストコードをブロック内に書いていきます
+  # ここにProductモデルのテストコードを書いていきます
+  pending "add some examples to (or delete) #{__FILE__}"
+end
