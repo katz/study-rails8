b42335ac0420142f8ef033844f889e84ee503a7b
Katz Sakai <sakai@onflow.jp>
2025-03-12 03:55:20 +0000

    argon2idでパスワードハッシュするようにしてみた

--- store/app/models/user.rb
+++ store/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_secure_password
+  include UserAuthByArgon2id
+
+  has_secure_password_by_argon2id
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
